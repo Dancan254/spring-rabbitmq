77b7a2733b5f0b066848c71aa80414578c69df3b
Dancan254 <dancanian25@gmail.com>
2026-06-26 12:59:28 +0300

    Initialize Spring RabbitMQ project with Maven setup and basic application structure

---
+++ src/test/java/com/javaguy/springrabbitmq/SpringRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguy.springrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
